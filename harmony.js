467bea12a43fa2e13474a4c5058f1f27405d29b9
Marcin Kumorek <kumeek@gmail.com>
2014-04-21 19:16:09 +0100

    Add harmony.js that can run over nodejs

---
+++ harmony.js
@@ -0,0 +1,20 @@
+'use strict'
+
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var log = console.log
+
+// var data
+// var httpReq = new XMLHttpRequest()
+// httpReq.onreadystatechange = function () {
+// 	if ( httpReq.readyState == 4 && httpReq.status == 200 ) {
+// 		console.log('package arrived!', foo)
+// 		data = JSON.parse(httpReq.responseText)
+// 		// console.log(data['MRData'].StandingsTable.StandingsLists[0].DriverStandings)
+// 		let standings = data['MRData'].StandingsTable.StandingsLists[0].DriverStandings
+// 		for (let driver of standings) {
+// 			console.log(driver.Driver.familyName)
+// 		}
+// 	}
+// }
+// httpReq.open('GET', 'http://ergast.com/api/f1/2013/driverStandings.json', true)
+// httpReq.send(null)
