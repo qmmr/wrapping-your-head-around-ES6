ab399854c74d72afc43ce5d823878ed64c8346e8
Marcin Kumorek <Marcin.Kumorek@Gamesys.co.uk>
2014-04-24 12:17:07 +0100

    Clean up node scripts

--- node-harmony.js
+++ node-harmony.js
@@ -2,13 +2,6 @@
 
 var log = console.log
 
-// var fs = require('fs');
-// var words = fs.readFileSync('./words').toString().split('\n');
-
-// for ( var word of words ) {
-//  console.log(word)
-// }
-
 // var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
 
--- traceur.js
+++ traceur.js
@@ -1,17 +1,23 @@
+// var fs = require('fs');
+// var words = fs.readFileSync('./words', { encoding: 'utf8' }).split('\n')
+// console.log(words)
+// for ( var word of words ) {
+//  console.log(word)
+// }
 
-var data
-var httpReq = new XMLHttpRequest()
-httpReq.onreadystatechange = function () {
-    if ( httpReq.readyState == 4 && httpReq.status == 200 ) {
-        data = JSON.parse(httpReq.responseText)
-        console.log(data['MRData'].StandingsTable.StandingsLists[0].DriverStandings[0])
-        let drivers = data['MRData'].StandingsTable.StandingsLists[0].DriverStandings
-        for (var driver of drivers) {
-            // console.log(driver.Driver.familyName)
-          let { givenName: firstName, familyName: lastName, nationality } = driver.Driver
-          console.log(firstName + ' ' + lastName + ' is ' + nationality)
-        }
-    }
-}
-httpReq.open('GET', 'http://ergast.com/api/f1/2013/driverStandings.json', true)
-httpReq.send(null)
+// var data
+// var httpReq = new XMLHttpRequest()
+// httpReq.onreadystatechange = function () {
+//     if ( httpReq.readyState == 4 && httpReq.status == 200 ) {
+//         data = JSON.parse(httpReq.responseText)
+//         console.log(data['MRData'].StandingsTable.StandingsLists[0].DriverStandings[0])
+//         let drivers = data['MRData'].StandingsTable.StandingsLists[0].DriverStandings
+//         for (var driver of drivers) {
+//             // console.log(driver.Driver.familyName)
+//           let { givenName: firstName, familyName: lastName, nationality } = driver.Driver
+//           console.log(firstName + ' ' + lastName + ' is ' + nationality)
+//         }
+//     }
+// }
+// httpReq.open('GET', 'http://ergast.com/api/f1/2013/driverStandings.json', true)
+// httpReq.send(null)
