ce082adb712e7dfb936df959fc7d31e7200f8c96
Marcin Kumorek <Marcin.Kumorek@Gamesys.co.uk>
2014-05-07 11:39:20 +0100

    Add gulp-uglify

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,20 @@
-var gulp   = require('gulp');
-var jshint = require('gulp-jshint');
-var es6ModuleTranspiler = require("gulp-es6-module-transpiler");
-var notify = require("gulp-notify");
+var gulp   = require('gulp')
+var jshint = require('gulp-jshint')
+var uglify = require('gulp-uglify')
+var notify = require("gulp-notify")
+var es6ModuleTranspiler = require("gulp-es6-module-transpiler")
 
 gulp.task('lint', function() {
   return gulp.src('./js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('jshint-stylish'));
 });
+
+gulp.task('uglify', function () {
+    return gulp.src('./register*.js')
+        .pipe(uglify())
+        .pipe(gulp.dest('./out'))
+})
 
 gulp.task('es6modules', function () {
     return gulp.src('./traceur-files/traceur.js')
