99fcb03a51a21065719ffe77e9c0791352454f66
Marcin Kumorek <kumeek@gmail.com>
2014-05-14 00:09:50 +0100

    Add more to Set section

--- math_test.js
+++ math_test.js
@@ -1,3 +1,11 @@
 var math = require('./lib/math.js')
 console.log( math.toFixed(3.14) )
 console.log( math.circumference( 6378 ) )
+
+var Library = {};
+var LOTR = { title: 'The Lord of The Rings', author: 'J.R.R Tolkien' };
+var TLP = { title: 'The Little Prince', author: 'Antoine de Saint-Exupery' };
+Library[LOTR] = 'The Lord of The Rings';
+Library[TLP] = 'The Little Prince';
+console.log( Library[LOTR], Library[TLP] ); // ???
+console.log( LOTR.toString() );
